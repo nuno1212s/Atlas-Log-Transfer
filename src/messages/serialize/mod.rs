3ac10f1b85377af381d6394b077a7ad31db9c264
Nuno Neto <nuno.neto.g@gmail.com>
2023-10-17 16:58:45 +0100

    Fixed febft compilation errors.

--- src/messages/serialize/mod.rs
+++ src/messages/serialize/mod.rs
@@ -2,6 +2,7 @@ use std::marker::PhantomData;
 use std::sync::Arc;
 use atlas_communication::message::Header;
 use atlas_communication::reconfiguration_node::NetworkInformationProvider;
+use atlas_common::ordering::Orderable;
 
 use atlas_core::log_transfer::networking::serialize::LogTransferMessage;
 use atlas_core::log_transfer::networking::signature_ver::LogTransferVerificationHelper;
@@ -22,42 +23,45 @@ impl<D: ApplicationData, OP: OrderingProtocolMessage<D>,
     LS: DecisionLogMessage<D, OP, POPT>> LogTransferMessage<D, OP> for LTMsg<D, OP, POPT, LS> {
     type LogTransferMessage = LTMessage<POPT::Proof, LS::DecLog>;
 
-    fn verify_log_message<NI, LVH>(network_info: &Arc<NI>, header: &Header, message: Self::LogTransferMessage) -> atlas_common::error::Result<(bool, Self::LogTransferMessage)>
+    fn verify_log_message<NI, LVH>(network_info: &Arc<NI>, header: &Header, message: Self::LogTransferMessage) -> atlas_common::error::Result< Self::LogTransferMessage>
         where NI: NetworkInformationProvider, LVH: LogTransferVerificationHelper<D, OP, NI>, {
-        match message.kind() {
+
+        let seq = message.sequence_number();
+
+        match message.into_kind() {
             LogTransferMessageKind::RequestLogState => {
-                Ok((true, message))
+                Ok(LTMessage::new(seq, LogTransferMessageKind::RequestLogState))
             }
             LogTransferMessageKind::ReplyLogState(opt) => {
                 if let Some((first_seq, (last_seq, proof))) = opt {
-                    let (result, proof) = POPT::verify_proof::<NI, LVH>(network_info, proof.clone())?;
+                    let proof = POPT::verify_proof::<NI, LVH>(network_info, proof.clone())?;
 
-                    Ok((result, message))
+                    Ok(LTMessage::new(seq, LogTransferMessageKind::ReplyLogState(Some((first_seq, (last_seq, proof))))))
                 } else {
-                    Ok((true, message))
+                    Ok(LTMessage::new(seq, LogTransferMessageKind::ReplyLogState(None)))
                 }
             }
-            LogTransferMessageKind::RequestProofs(_) => {
-                Ok((true, message))
+            LogTransferMessageKind::RequestProofs(seqs) => {
+                Ok(LTMessage::new(seq, LogTransferMessageKind::RequestProofs(seqs)))
             }
             LogTransferMessageKind::ReplyLogParts( proofs) => {
-                for (seq, proof) in proofs {
-                    let (result, proof) = POPT::verify_proof::<NI, LVH>(network_info, proof.clone())?;
+                let mut proofs_cpy = Vec::with_capacity(proofs.len());
 
-                    if !result {
-                        return Ok((false, message));
-                    }
+                for (seq, proof) in proofs {
+                    let proof = POPT::verify_proof::<NI, LVH>(network_info, proof.clone())?;
+
+                    proofs_cpy.push((seq, proof));
                 }
 
-                Ok((true, message))
+                Ok(LTMessage::new(seq, LogTransferMessageKind::ReplyLogParts(proofs_cpy)))
             }
             LogTransferMessageKind::RequestLog => {
-                Ok((true, message))
+                Ok(LTMessage::new(seq, LogTransferMessageKind::RequestLog))
             }
             LogTransferMessageKind::ReplyLog(dec_log) => {
-                let (result, dec_log) = LS::verify_decision_log::<NI, LVH>(network_info, dec_log.clone())?;
+                let dec_log = LS::verify_decision_log::<NI, LVH>(network_info, dec_log.clone())?;
 
-                Ok((result, message))
+                Ok(LTMessage::new(seq, LogTransferMessageKind::ReplyLog(dec_log)))
             }
         }
     }
